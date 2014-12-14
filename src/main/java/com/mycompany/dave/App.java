738fbc06e233da7944fe05f09199d7616049ec57
DaveNFS <bill.dave.utah@gmail.com>
2014-12-13 20:40:27 -0700

    none

--- src/main/java/com/mycompany/dave/App.java
+++ src/main/java/com/mycompany/dave/App.java
@@ -1,6 +1,6 @@
 package com.mycompany.dave;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 
 public class App 
@@ -8,11 +8,17 @@ public class App
     public static void main( String[] args ) throws Exception
     {
         TerminologyLookUp LU = new TerminologyLookUp();
-        ArrayList<String> ans = LU.lookUp("C0032344", 5);
-        for(String x:ans)
+//        ArrayList<String> ans = LU.lookUp("C0032344", 5);
+//        for(String x:ans)
+//        {
+//        	System.out.println(x);
+//        }	
+        
+        HashSet<String> ans2 = LU.retrivePreferredName("Mild mental retardation");
+        for(String x:ans2)
         {
         	System.out.println(x);
-        }	
+        }
      
     }
 }
--- src/main/java/com/mycompany/dave/TerminologyLookUp.java
+++ src/main/java/com/mycompany/dave/TerminologyLookUp.java
@@ -5,13 +5,20 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class TerminologyLookUp {
 
 	 public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	 public static final String DB_URL = "jdbc:mysql://umls.effectivedynamics.com";
-	 public static final String USER_NAME = "umls_ro";
-	 public static final String PASSWORD = "s4#fde2";
+//	 public static final String DB_URL = "jdbc:mysql://umls.effectivedynamics.com";
+//	 public static final String USER_NAME = "umls_ro";
+//	 public static final String PASSWORD = "s4#fde2";
+	 
+	 //------
+	 public static final String DB_URL = "jdbc:mysql://mysql.chpc.utah.edu";
+	 public static final String USER_NAME = "umlsro";
+	 public static final String PASSWORD = "umls";
+	 
 	 
 	 
 	
@@ -46,16 +53,81 @@ public class TerminologyLookUp {
 			 }
 		}
 	    
-	    
-		
-	    
-	    
-	    
+	       
 	    
 	    // close connection
 	    conn.close();
 		
 		return output;
 	}
+	
+	
+	
+	public HashSet<String> retrivePreferredName(String concept) throws Exception
+	{
+		HashSet<String> output = new HashSet<String>();
+		String conceptCUI = "";
+		
+		// First find the CUI for input string:
+		Class.forName(JDBC_DRIVER);
+		Connection  conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+		
+		Statement stmt = conn.createStatement();
+		String sql = "SELECT CUI FROM umls.MRCONSO WHERE STR='" + concept + "' LIMIT 1;";
+		ResultSet res = stmt.executeQuery(sql);
+		
+	   while(res.next())
+		 {
+			 conceptCUI= res.getString("CUI");
+		 }
+	
+	    // close response
+	    res.close();
+	    
+	    // now get str from def of all the related CUIs
+	    stmt = conn.createStatement();	
+		
+	    // related concepts: 
+	    ArrayList<String> relatedCUIs = new ArrayList<String>();
+	    
+	    // now get all related CUIs
+	    stmt = conn.createStatement();
+	    sql = "SELECT CUI2 FROM umls.MRREL WHERE CUI1='"+ conceptCUI + "';";
+		res = stmt.executeQuery(sql);
+		while(res.next())
+		{
+				 relatedCUIs.add(res.getString("CUI2"));
+		}
+		
+		// now get the preferred names for the CUIs
+		
+		for(String x:relatedCUIs)
+		{
+		    stmt = conn.createStatement();
+		    sql = "SELECT STR FROM umls.MRCONSO WHERE CUI='" + x + "' AND ISPREF='Y' AND LAT='ENG';";
+			res = stmt.executeQuery(sql);
+			while(res.next())
+			{
+					 output.add(res.getString("STR"));
+			}
+		
+		}
+		
+
+	    // close connection
+	    conn.close();
+	    
+	   
+		return output;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
